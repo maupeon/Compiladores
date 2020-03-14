8000b6eda5c95e4ef1bf3e7b52eae9187dd7b171
AlexandroMarcelo <alex.fcomarcelo@gmail.com>
2020-03-14 15:25:14 -0600

    transition table NFA working but with more epsilons

--- Tarea 1/tarea1.py
+++ Tarea 1/tarea1.py
@@ -6,6 +6,11 @@ Andres Campos Tams
 Tarea 1: RE -> NFA -> DFA
 '''
 
+# Declaration of global constants
+STARTNODE=0
+FINALNODE=1
+TOKEN=2
+
 # Class to convert the regular expression in infix to postfix
 class Conversion: 
     # Constructor
@@ -78,8 +83,10 @@ class Automata():
         self.REPostfix=[]
         # For storing tempoarlly the evaluated symbols
         self.stack=[]
-        # For the transition table
-        self.transitionTable=[]
+        # For the transition table of the NFA
+        self.NFA=[]
+        # For the number of nodes in the NFA
+        self.N=0
 
     # Read the file and store the Regular expression
     def readFile(self,filename):
@@ -93,38 +100,100 @@ class Automata():
             f.close() 
         except:
             print("ERROR file not found")
-
+    
     # Call the Conversion class which going to convert from infix to postfix the RE
     def convertREToPostfix(self):
         conversion = Conversion()
         self.REPostfix = conversion.infixToPostfix(self.RE)
         print(self.REPostfix)
 
+    # Auxiliar functions to make a cleaner code to retrieve information of the transition table
+    def startNodeTransition(self, transition):
+        return transition[STARTNODE]
+    def finalNodeTransition(self, transition):
+        return transition[FINALNODE]
+    def transitionToken(self, transition):
+        return transition[TOKEN]
+
+    def symbolEvaluation(self, token):
+        startNode = self.N
+        finalNode = self.N+1
+        self.stack.append([startNode, finalNode, token])
+        self.NFA.append([startNode, finalNode, token])
+        self.N = self.N+2
+
+    def unionEvaluation(self):
+        A = self.stack[len(self.stack)-2]
+        B = self.stack[len(self.stack)-1]
+        if self.transitionToken(A) == '|':
+            startNode = self.startNodeTransition(A)
+            finalNode = self.finalNodeTransition(A)
+            self.NFA.append([startNode, self.startNodeTransition(B),'e'])
+            self.NFA.append([self.finalNodeTransition(B), finalNode,'e'])
+        else:
+            startNode = self.N
+            finalNode = startNode+1
+            self.NFA.append([startNode, self.startNodeTransition(A),'e'])
+            self.NFA.append([startNode, self.startNodeTransition(B),'e'])
+            self.NFA.append([self.finalNodeTransition(A), finalNode,'e'])
+            self.NFA.append([self.finalNodeTransition(B), finalNode,'e'])
+            self.N = self.N+2
+        self.stack.append([startNode, finalNode, '|'])
+
+    def kleeneStarEvaluation(self):
+        A = self.stack[len(self.stack)-1]
+        startNode = self.N
+        finalNode = startNode+1
+        self.NFA.append([startNode, self.startNodeTransition(A),'e'])
+        self.NFA.append([self.finalNodeTransition(A), finalNode,'e'])
+        self.NFA.append([self.finalNodeTransition(A), self.startNodeTransition(A),'e'])
+        self.NFA.append([startNode, finalNode,'e'])
+        self.N = self.N+2
+        self.stack.append([startNode, finalNode, '*'])
+    
+    def concatenationEvaluation(self):
+        A = self.stack[len(self.stack)-2]
+        B = self.stack[len(self.stack)-1]
+        startNode = self.startNodeTransition(A)
+        finalNode = self.finalNodeTransition(B)
+        self.NFA.append([self.finalNodeTransition(A), self.startNodeTransition(B),'e'])
+        self.stack.append([startNode, finalNode, '.'])
+        '''
+        if self.transitionToken(A) == '.':
+            pass
+        else:
+            self.stack.append([startNode, finalNode, '.'])
+        self.N = self.N-1
+        '''
+
     # Function to convert the regular expression into a NFA
     def convertREToNFA(self):
         for token in self.REPostfix:
             if token == '.':
-                pass
                 #A = self.stack.pop()
                 #B = self.stack.pop()
                 # Call a concatenation function
+                self.concatenationEvaluation()
             elif token == '|':
-                pass
                 #A = self.stack.pop()
                 #B = self.stack.pop()
                 # Call a union function
+                self.unionEvaluation()
             elif token == '*':
-                pass
                 #A = self.stack.pop()
                 # Call a kleene star function
+                self.kleeneStarEvaluation()
             elif token == '+':
                 pass
                 #A = self.stack.pop()
                 # Call a one or more function
             else:
-                pass
                 #evaluate the current symbol
-            
+                self.symbolEvaluation(token)
+        print()
+        print("The transition table of the NFA is:",self.NFA)
+        print()
+        print("The stack is:",self.stack)
 
 if __name__ == "__main__":
     automata = Automata()
